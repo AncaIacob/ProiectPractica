843c64c143ee528e4331b5bb23cce0b875ac869d
CezaraDulceac <cezara_sv@yahoo.com>
2020-08-10 23:51:31 +0300

    Client/Server Socket

--- include/ClientSocket.cpp
+++ include/ClientSocket.cpp
@@ -2,13 +2,37 @@
 #include <iostream>
 
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h> 
+#include <netinet/in.h>
+#include <string.h>
+#define SIZEREAD 1024
 
 ClientSocket::ClientSocket(std::string address, int port)
 {
-    
+    m_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(m_fd == -1)
+    {
+        perror("Couldn't create socket");
+        exit(EXIT_FAILURE);
+    }
 
+    serv_addr.sin_family = AF_INET; //Internet Protocol v4 addresses
+    serv_addr.sin_port = htons(port);
+
+    //converts text to binary form 
+    if(inet_pton(AF_INET, address.c_str(), &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        exit(EXIT_FAILURE);
+    } 
+
+}
+
+ClientSocket::ClientSocket(int fd)
+{
+    m_fd = fd;
 }
 ClientSocket::~ClientSocket()
 {
@@ -17,19 +41,29 @@ ClientSocket::~ClientSocket()
 
 ClientSocket::operator bool()
 {  
- return m_fd != 0; 
+    return m_fd != 0; 
 }
 
 bool ClientSocket::connect()
 {
-
+    if (::connect(m_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return false; 
+    } 
+    return true;
 }
 
-void ClientSocket::send(std::vector<std::byte> message)
+void ClientSocket::send(std::vector<std::byte> &message)
 {
-
+    ::send(m_fd , &message , message.size() , 0 );
+    std::cout << "Message sent from client\n";
 }
-void ClientSocket::receive(std::vector<std::byte> message)
-{
 
+void ClientSocket::receive()
+{
+    char buffer[SIZEREAD] = {0};
+    int readStuff = ::read(m_fd, buffer, SIZEREAD);
+    std::cout << "Message read (" << readStuff << " characters) from server:\n";
+    std::cout << buffer << std::endl;
 }
--- include/ServerSocket.cpp
+++ include/ServerSocket.cpp
@@ -1,16 +1,50 @@
 #include "ServerSocket.hpp"
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
 
-ServerSocket::ServerSocket(std::string address, int port)
+ServerSocket::ServerSocket(int port)
 {
+    if ((m_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    int opt = 1;
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(m_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    m_address.sin_family = AF_INET; 
+    m_address.sin_addr.s_addr = INADDR_ANY; 
+    m_address.sin_port = htons(port); 
 
 }
 
 ServerSocket::~ServerSocket()
 {
-    
+    close(m_fd);
 }
 
 ClientSocket ServerSocket::accept()
 {
-    
+    int newSocket;
+    int addrlen = sizeof(m_address); 
+
+    if ((newSocket = ::accept(m_fd, (struct sockaddr *)&m_address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    return ClientSocket(newSocket);
 }
